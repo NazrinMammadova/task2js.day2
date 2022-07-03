9f34b691d0ebff98118ec5f9a328fc49f037c68c
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-07-03 22:20:09 +0400

    third task day2 in js

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+var body = document.body;
+var icon = document.getElementById('mode')
+function P324(){
+
+
+
+
+
+if(body.style.backgroundColor === 'black'){
+body.style.backgroundColor = 'white';
+body.style.color = 'black';
+icon.className = 'fa-solid fa-sun';
+
+}
+
+else{
+    body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+        icon.className = 'fa-solid fa-sun';
+ 
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
